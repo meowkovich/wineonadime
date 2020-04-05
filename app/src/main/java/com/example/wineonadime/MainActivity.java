562b03d9d9240612a989218795b3f7287841b302
AllisonWinter-Reed <winterreed@wisc.edu>
2020-04-05 10:14:16 -0500

    Bottom Navigation Menu -Fix Search Text appearing below image

--- app/src/main/java/com/example/wineonadime/MainActivity.java
+++ app/src/main/java/com/example/wineonadime/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.wineonadime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_guide:
+                            openFragment(GuideFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_map:
+                            openFragment(MapFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_search:
+                            openFragment(SearchFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
     public void goToMap( View view )
     {
