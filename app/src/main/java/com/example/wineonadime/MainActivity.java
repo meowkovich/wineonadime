8f01348cbee6f230359ceffe99f4f1ac6914ad8e
AllisonWinter-Reed <winterreed@wisc.edu>
2020-04-12 14:01:46 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/wineonadime/MainActivity.java
+++ app/src/main/java/com/example/wineonadime/MainActivity.java
@@ -1,31 +1,43 @@
 package com.example.wineonadime;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
+    SharedPreferences userLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        userLog = getSharedPreferences("wineOnADimeLogin", Context.MODE_PRIVATE);
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         bottomNavigation.setSelectedItemId(R.id.navigation_home);
-        openFragment(HomeFragment.newInstance("", ""));
+      //  hideBottomBar(false);
+
+        openFragment(LoginFragment.newInstance("",""));
+    }
+
+    public void hideBottomBar(boolean isHidden){
+        bottomNavigation.setVisibility(isHidden ? View.GONE : View.VISIBLE);
     }
 
     public void openFragment(Fragment fragment) {
@@ -58,6 +70,62 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 }
             };
+
+    public void signUp(View view) {
+        openFragment(RegisterFragment.newInstance("",""));
+
+    }
+
+    public void login(View view) {
+        EditText username_text = findViewById(R.id.username);
+        EditText password_text = findViewById(R.id.password);
+        TextView error_message = findViewById(R.id.loginInfoText);
+
+        String username = username_text.getText().toString();
+        String password = password_text.getText().toString();
+
+        if(userLog.contains(username)) {
+            if(userLog.getString(username,"").equals(password)) {
+                openFragment(HomeFragment.newInstance("", ""));
+            }
+            else {
+                error_message.setText(getResources().getString(R.string.incorrectPassword));
+                error_message.setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        }
+        else{
+            error_message.setText(getResources().getString(R.string.usernameDoesNotExist));
+            error_message.setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
+    }
+
+    public void continueAsGuest(View view) {
+
+        openFragment(HomeFragment.newInstance("", ""));
+    }
+
+    public void register(View view) {
+        EditText username_text = findViewById(R.id.username_register);
+        EditText email_text = findViewById(R.id.email_register);
+        EditText password_text = findViewById(R.id.password_register);
+        TextView error_message = findViewById(R.id.registerInfoText);
+
+        String username = username_text.getText().toString();
+        String email = email_text.getText().toString();
+        String password = password_text.getText().toString();
+
+        if(userLog.contains(username)) {
+            error_message.setText(getResources().getString(R.string.usernameExists));
+            error_message.setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
+        else {
+            
+        }
+
+
+        openFragment(LoginFragment.newInstance("",""));
+
+    }
 
 //    public void goToMap( View view )
 //    {
